0b4e5c77023d671bc59880620c001a119ec83cb5
L4falafa <70497466+L4falafa@users.noreply.github.com>
2020-09-10 16:58:06 -0300

    Update EasyMedicConfig.cs

--- EasyMedicConfig.cs
+++ EasyMedicConfig.cs
@@ -1,4 +1,4 @@
-﻿using Rocket.API;
+using Rocket.API;
 using SDG.Unturned;
 
 namespace Lafalafa.L4EasyMedicalSystem
@@ -9,10 +9,13 @@ namespace Lafalafa.L4EasyMedicalSystem
         public int DesfribilatorId { get; set; }
         public string ImageUrl { get; set; }
 
+        public bool LockScreen { get; set; }
+
         public void LoadDefaults()
         {
             DesfribilatorId = 15;
             ImageUrl = "https://cdn.discordapp.com/attachments/661993286046711808/753380017903370311/latido-del-corazon.png";
+            LockScreen = false;
         }
 
     }
